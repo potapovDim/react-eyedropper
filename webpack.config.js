07cc4ba240b7a05e0b0d95df8a6468310f8b1e6e
potapovDim <dmytrop@weblium.com>
2017-03-11 13:39:02 +0200

    refactor

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: "index.js"
+  },
+  devServer: {
+    port: 7070,
+    historyApiFallback: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        }
+      }
+    ]
+  }
+}
