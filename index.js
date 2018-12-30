3412307304181c4efb2e5529457cdabcad35fa02
gundamftw <test@test.com>
2018-12-30 01:51:46 -0500

    - update PropType syntax - update dependencies

--- index.js
+++ index.js
@@ -1,98 +1,126 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.default = void 0;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _react = _interopRequireWildcard(require("react"));
 
-var _react = require('react');
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _react2 = _interopRequireDefault(_react);
+var _html2canvas = _interopRequireDefault(require("html2canvas"));
 
-var _html2canvas = require('html2canvas');
-
-var _html2canvas2 = _interopRequireDefault(_html2canvas);
-
-var _getCanvasPixelColor2 = require('get-canvas-pixel-color');
-
-var _getCanvasPixelColor3 = _interopRequireDefault(_getCanvasPixelColor2);
+var _getCanvasPixelColor2 = _interopRequireDefault(require("get-canvas-pixel-color"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var EyeDropper = function (_Component) {
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var EyeDropper =
+/*#__PURE__*/
+function (_Component) {
   _inherits(EyeDropper, _Component);
 
   function EyeDropper() {
-    var _ref;
+    var _getPrototypeOf2;
 
-    var _temp, _this, _ret;
+    var _this;
 
     _classCallCheck(this, EyeDropper);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = EyeDropper.__proto__ || Object.getPrototypeOf(EyeDropper)).call.apply(_ref, [this].concat(args))), _this), _this.eyeDropper = function (e) {
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(EyeDropper)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "eyeDropper", function (e) {
       var initializedColor = _this.props.initializedColor;
 
       var removeEventListener = function removeEventListener() {
         document.removeEventListener('click', _this.eyeDropper);
       };
-      (0, _html2canvas2.default)(e.toElement, {
+
+      (0, _html2canvas.default)(e.toElement, {
         onrendered: function onrendered(canvas) {
           var x = e.offsetX == undefined ? e.layerX : e.offsetX;
           var y = e.offsetY == undefined ? e.layerY : e.offsetY;
 
-          var _getCanvasPixelColor = (0, _getCanvasPixelColor3.default)(canvas, x, y),
+          var _getCanvasPixelColor = (0, _getCanvasPixelColor2.default)(canvas, x, y),
               r = _getCanvasPixelColor.r,
               g = _getCanvasPixelColor.g,
               b = _getCanvasPixelColor.b,
               a = _getCanvasPixelColor.a;
 
-          a === 0 ? initializedColor({ r: 255, g: 255, b: 255 }) : initializedColor({ r: r, g: b, b: g });
+          a === 0 ? initializedColor({
+            r: 255,
+            g: 255,
+            b: 255
+          }) : initializedColor({
+            r: r,
+            g: b,
+            b: g
+          });
           removeEventListener();
         }
       });
       document.body.style.cursor = 'default';
-    }, _this.initEyeDropper = function (event) {
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "initEyeDropper", function (event) {
       _this.props.onInit && _this.props.onInit();
       document.body.style.cursor = 'cell';
       document.addEventListener('click', _this.eyeDropper);
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    });
+
+    return _this;
   }
 
   _createClass(EyeDropper, [{
-    key: 'render',
+    key: "render",
     value: function render() {
-      var _props = this.props,
-          className = _props.className,
-          title = _props.title;
-
+      var _this$props = this.props,
+          className = _this$props.className,
+          title = _this$props.title;
       var classNameComponent = className ? className : "eye dropper";
       var titleComponent = title ? title : '+';
-      return _react2.default.createElement(
-        'div',
-        { className: classNameComponent, onClick: this.initEyeDropper },
-        titleComponent
-      );
+      return _react.default.createElement("div", {
+        className: classNameComponent,
+        onClick: this.initEyeDropper
+      }, titleComponent);
     }
   }]);
 
   return EyeDropper;
 }(_react.Component);
 
-EyeDropper.propTypes = {
-  title: _react.PropTypes.string,
-  className: _react.PropTypes.string,
-  initializedColor: _react.PropTypes.func.isRequired,
-  onInit: _react.PropTypes.func
-};
-exports.default = EyeDropper;
+_defineProperty(EyeDropper, "propTypes", {
+  title: _propTypes.default.string,
+  className: _propTypes.default.string,
+  initializedColor: _propTypes.default.func.isRequired,
+  onInit: _propTypes.default.func
+});
+
+var _default = EyeDropper;
+exports.default = _default;
--- src/app/eye-dropper.js
+++ src/app/eye-dropper.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import html2canvas from 'html2canvas'
 import getCanvasPixelColor from 'get-canvas-pixel-color'
 
